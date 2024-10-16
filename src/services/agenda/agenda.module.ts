c0ac677b0c9f6e625b6218138718b075383a5d07
Soris Anish <125728458+SorisAnish17@users.noreply.github.com>
2024-10-16 19:33:47 +0400

    Delete src directory

--- src/services/agenda/agenda.module.ts
+++
@@ -1,9 +0,0 @@
-// agenda.module.ts
-import { Module } from '@nestjs/common';
-import { AgendaProvider } from './agenda.provider';
-
-@Module({
-  providers: [AgendaProvider],
-  exports: [AgendaProvider], // Ensure it's exported
-})
-export class AgendaModule {}
