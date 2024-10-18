cb792d16342cafa82394f2046da0c9bd28d19dc0
Soris Anish <125728458+SorisAnish17@users.noreply.github.com>
2024-10-18 19:42:09 +0400

    Delete src directory

--- src/libs/database/index.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseClientService } from './index.service';
-
-@Module({
-  providers: [DatabaseClientService],
-
-  exports: [DatabaseClientService],
-})
-export class DatabaseClientModule {}
