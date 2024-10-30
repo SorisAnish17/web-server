ec7854782baf73cc5032b5c6746900bf2ed49079
Soris Anish <125728458+SorisAnish17@users.noreply.github.com>
2024-10-30 20:29:51 +0400

    Delete src directory

--- src/libs/email-delivery-client/email-delivery.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmailClientService } from './email-delivery.service';
-
-@Module({
-  providers: [EmailClientService],
-  exports: [EmailClientService],
-})
-export class EmailClientModule {}
